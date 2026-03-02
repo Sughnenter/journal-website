2c80ff03daa61aa6cc27f3f7d5178f44a3d01370
shammah <shamaj2006@gmail.com>
2026-03-02 11:46:31 +0100

    setup project folder

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import "./Footer.css";
+import gpt3Logo from "../../assets/logo.svg";
+function Footer() {
+  return (
+    <div className="gpt3__footer section__padding reveal">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text reveal" data-animation="fade-right">
+          Do you want to step in to the future before others
+        </h1>
+      </div>
+
+      <div className="gpt3__footer-btn reveal" data-animation="fade-right">
+        <p>Request Early Access</p>
+      </div>
+
+      <div className="gpt3__footer-links">
+        <div
+          className="gpt3__footer-links_logo reveal"
+          data-animation="fade-left"
+        >
+          <img src={gpt3Logo} alt="gpt3_logo" />
+          <p>
+            Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved
+          </p>
+        </div>
+        <div
+          className="gpt3__footer-links_div reveal"
+          data-animation="fade-right"
+        >
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div
+          className="gpt3__footer-links_div reveal"
+          data-animation="fade-right"
+        >
+          <h4>Company</h4>
+          <p>Terms & Conditions </p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div
+          className="gpt3__footer-links_div reveal"
+          data-animation="fade-right"
+        >
+          <h4>Get in touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132567</p>
+          <p>info@payme.net</p>
+        </div>
+      </div>
+
+      <div
+        className="gpt3__footer-copyright reveal"
+        data-animation="fade-right"
+      >
+        <p>@2021 GPT-3. All rights reserved.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
