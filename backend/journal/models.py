439fb8a83e05aba29a53a95b778b1d206d264602
shammah <shamaj2006@gmail.com>
2026-03-10 18:57:02 +0100

    added articles model and created url patterns

--- backend/journal/models.py
+++ backend/journal/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Journal(models.Model):
+    AUTHOR_TITLE_CHOICES=(
+        ('mr','MR'),
+        ('dr','DR'),
+        ('mrs', 'MRS')
+        )
+    Title = models.TextField(max_length=200)
+    Abstract = models.TextField(max_length=300)
+    Author_title = models.CharField()
+    Author = models.TextField(choices=AUTHOR_TITLE_CHOICES)
+
+    class Meta:
+        verbose_name = _("Journal")
+        verbose_name_plural = _("Journals")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Journal_detail", kwargs={"pk": self.pk})
