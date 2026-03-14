1e9912f8bbdb140ac3ed00a526e3509841106657
shammah <shamaj2006@gmail.com>
2026-03-14 11:56:17 +0100

    added volume and category model

--- backend/journal/models.py
+++ backend/journal/models.py
@@ -6,23 +6,40 @@ from django.core.validators import FileExtensionValidator
 User = get_user_model
 
 # Create your models here.
-class Journal(models.Model):
-    AUTHOR_TITLE_CHOICES=(
-        ('mr','MR'),
-        ('dr','DR'),
-        ('mrs', 'MRS')
-        )
-    Title = models.TextField(max_length=200)
-    Abstract = models.TextField(max_length=300)
-    Author_title = models.CharField()
-    Author = models.TextField(choices=AUTHOR_TITLE_CHOICES)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        verbose_name = _("Journal")
-        verbose_name_plural = _("Journals")
+        verbose_name_plural = "Categories"
+        ordering = ['name']
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
-        return reverse("Journal_detail", kwargs={"pk": self.pk})
+
+class Volume(models.Model):
+    number = models.IntegerField(unique=True)
+    year = models.IntegerField()
+    description = models.TextField(blank=True)
+    is_current = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-number']
+
+    def __str__(self):
+        return f"volume {self.number} ({self.year})"
+
+    def save(self, *args, **kwargs):
+        if self.is_current:
+            Volume.objects.filter(is_current=True).update(is_current=False)
+        super().save(*args, **kwargs)
+
+class Issue(models.Model)
