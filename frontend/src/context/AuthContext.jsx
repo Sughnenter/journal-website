3dc0bc5ef4984c2b3aeada25d89900a723dcb7d9
shammah <shamaj2006@gmail.com>
2026-03-22 13:21:28 +0100

    added protected route and auth context

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,56 @@
+import React, {createContext, useState, useContext, useEffect} from "react";
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem('access_token');
+    const userData = localStorage.getItem('user_data');
+
+    if (token && userData) {
+      setUser(JSON.parse(userData));
+    }
+    setLoading(false);
+  }, []);
+
+  const login = (userData, tokens) => {
+    localStorage.setItem('access_token', tokens.access);
+    localStorage.setItem('refresh_token', tokens.refresh);
+    localStorage.setItem('user_data', JSON.stringify(userData));
+    setUser(userData);
+  };
+
+  const logout = () => {
+    localStorage.removeItem('acess_token');
+    localStorage.removeItem('refresh_token');
+    localStorage.removeItem('userData');
+    setUser(null);
+  };
+
+  const updateUser = (userData) => {
+    localStorage.setItem('user_data', JSON.stringify(userData));
+    setUser(userData);
+  };
+
+  const value ={
+    user,
+    login,
+    logout,
+    updateUser,
+    isAuthenticated: !!user,
+    loading,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context){
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
